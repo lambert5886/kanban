b6d7fa8bfdd6b5ec95fe143861cbb03695adc20c
lambert886 <lambert886@163.com>
2018-08-21 22:24:55 +0800

    添加富文本编辑器

---
+++ src/App3.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app" class="layout">
+    <editor class="editor" 
+    :value="content"  
+    :setting="editorSetting"
+    id='tinymce'
+     @input="(content)=> content = content"></editor>
+       
+  </div>
+</template>
+<script>
+    import editor from '@/components/editor/editor.vue'
+    export default {
+        name: "editor-demo",
+        data: function () {
+            return {
+                content:'我是富文本编辑器的内容',
+                //tinymce的配置信息 参考官方文档 https://www.tinymce.com/docs/configure/integration-and-setup/
+                editorSetting:{
+                    height:400,
+                }
+            }
+        },
+        components:{
+            'editor':editor
+        }
+    }
+</script>
